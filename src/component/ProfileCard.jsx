767382d24da961d8796ce7d4bf83106ead5038c0
Kr-Verified <dmschd0573@gmail.com>
2025-08-07 18:09:21 +0900

    docs :: ProfileCard 변경

--- src/component/ProfileCard.jsx
+++ src/component/ProfileCard.jsx
@@ -14,24 +14,21 @@ export default function ProfileCard({ profileList, setProfileList, profile, inde
   }
 
   return (
-    <div>
-      <h1>프로필 카드 목록</h1>
-      <div key={index} style={{ border: '3px solid black', borderRadius: '30px', padding: '25px', margin: '50px', backgroundColor: '#fffbde', fontSize: '35px'}}>
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-          <div style={{marginRight: '20px'}}><img src={getProfileImg(profile.imgUrl)} alt={profile.name} /></div>
-          <div>
-            <p><b>Team. {profile.team}</b></p>
-            <p><b>{profile.job}</b></p>
-            <p><b>tel.</b> {profile.tel}</p>
-            <p><b>email.</b> {profile.email}</p>
-          </div>
+    <div key={index} style={{ border: '3px solid black', borderRadius: '30px', padding: '25px', margin: '50px', backgroundColor: '#fffbde', fontSize: '35px'}}>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <div style={{marginRight: '20px'}}><img src={getProfileImg(profile.imgUrl)} alt={profile.name} /></div>
+        <div>
+          <p><b>Team. {profile.team}</b></p>
+          <p><b>{profile.job}</b></p>
+          <p><b>tel.</b> {profile.tel}</p>
+          <p><b>email.</b> {profile.email}</p>
         </div>
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-          <h2 style={{marginTop: 0}}>{profile.name}</h2>
-          <div style={{marginRight: '75px'}}>
-            <button style={{marginRight: '100px', scale: 2.5, backgroundColor: 'blue', borderRadius: '7px', border: '1px solid black'}}>수정</button>
-            <button style={{marginRight: '50px', scale: 2.5, backgroundColor: 'red', borderRadius: '7px', border: '1px solid black'}} onClick={() => delProfile(index)}>삭제</button>
-          </div>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <h2 style={{marginTop: 0}}>{profile.name}</h2>
+        <div style={{marginRight: '75px'}}>
+          <button style={{marginRight: '100px', scale: 2.5, backgroundColor: 'blue', borderRadius: '7px', border: '1px solid black'}}>수정</button>
+          <button style={{marginRight: '50px', scale: 2.5, backgroundColor: 'red', borderRadius: '7px', border: '1px solid black'}} onClick={() => delProfile(index)}>삭제</button>
         </div>
       </div>
     </div>
